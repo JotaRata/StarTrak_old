df27eb56cf4741dffeb8522e4f8333e6425d2259
Cefania <46698244+Cefania@users.noreply.github.com>
2019-01-25 20:28:28 -0300

    Update backgroundEstimator.py

--- STCore/utils/backgroundEstimator.py
+++ STCore/utils/backgroundEstimator.py
@@ -1,5 +1,7 @@
 from astropy.stats import SigmaClip
 from photutils import Background2D, ModeEstimatorBackground
+import numpy as np
+
 
 def GetBackground(image):
     sigma_clip = SigmaClip(sigma=3.)
@@ -7,5 +9,5 @@ def GetBackground(image):
     bkg = Background2D(image, (20, 30), filter_size=(4, 4),sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
     #plt.imshow(bkg.background, origin='lower', cmap='Greys_r')
     background=bkg.bkg_estimator.calc_background(image)
-    sig=np.std(bkg.background)
+    sig=np.std(image)
     return background,sig
