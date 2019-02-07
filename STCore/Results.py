41e345a5ba9e8f63bd6f3e58763d4aac3eedf6a2
JotaRata <jjbarria.c@gmail.com>
2019-02-06 23:53:31 -0300

    Update Results.py

--- STCore/Results.py
+++ STCore/Results.py
@@ -10,7 +10,8 @@ from STCore.utils.backgroundEstimator import GetBackground
 ResultsFrame = None
 
 #endregion
-# Funcion Awake, crea los objetos de tkinter
+
+
 def Awake(root, ItemList, TrackedStars):
 	global ResultsFrame
 	STCore.DataManager.CurrentWindow = 4
@@ -19,41 +20,39 @@ def Awake(root, ItemList, TrackedStars):
 	CreateCanvas(ResultsFrame, ItemList, TrackedStars)
 	
 
-def constant(ItemList,trackedPos,radius,Ref):
-	pos = list(reversed(trackedPos[0]))
+def GetConstant(ItemList, TrackedStars, StarIndex, Ref):
+	track = TrackedStars[StarIndex]
+	pos = list(reversed(track.trackedPos[0]))
+	radius = track.star.radius
 	data = ItemList[0].data
 	clipLoc = numpy.clip(pos, radius, (data.shape[0] - radius, data.shape[1] - radius))
 	crop = data[clipLoc[0]-radius : clipLoc[0]+radius,clipLoc[1]-radius : clipLoc[1]+radius]
-	co=Ref+2.5*numpy.log10(numpy.sum(crop)/(radius**2)-GetBackground(data)[0])
-	print co
-	return co
+	StarFlux = numpy.sum(crop) / (radius ** 2)
+	BackgroundFlux = GetBackground(data)[0]
+	value = Ref + 2.5 * numpy.log10(StarFlux - BackgroundFlux)
+	return value
 
-# Esta funcion crea los objetos de matplotlib, desde la linea 23 hasta la 29 es igual que cualquier otro script
+
 def CreateCanvas(app, ItemList, TrackedStars):
 	viewer = tk.Frame(app, width = 700, height = 400, bg = "white")
-	viewer.pack(side=tk.LEFT, fill = tk.BOTH, expand = True, anchor = tk.W)	  # Modifica desde aqui
+	viewer.pack(side=tk.LEFT, fill = tk.BOTH, expand = True, anchor = tk.W)
 	fig = figure.Figure(figsize = (7,4), dpi = 100)
 	ax = fig.add_subplot(111)
 	XAxis = range(len(ItemList))
-#calculo de la constante, se ingresa la magnitud y se debe seleccionar la estrella correcta
-	for s in TrackedStars:
-		c=constant(ItemList,s.trackedPos,s.star.radius,0.0)
-		break
+
+	c = GetConstant(ItemList, TrackedStars, 0, 0.0)
+
 	for t in TrackedStars:
-		YAxis = GetTrackedValue(ItemList, t.trackedPos, t.star.radius,c)
-		print YAxis
+		YAxis = GetTrackedValue(ItemList, t.trackedPos, t.star.radius, c)
 		Plot = ax.scatter(XAxis, YAxis)
+
 	ax.invert_yaxis()	
-	PlotCanvas = FigureCanvasTkAgg(fig,master=viewer)                        # Hasta aqui
+	PlotCanvas = FigureCanvasTkAgg(fig,master=viewer)
 	PlotCanvas.draw()
 	PlotCanvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 	return PlotCanvas
 
-# Esta funcion SOLO toma el brillo maximo de la estrella
-# Utiliza los parametros ItemList: Lista de archivos (objetos FileItem), trackedPos: Lista de posiciones (fil, col)
-# radius que es el valor dado por StarItem.radius (mira la linea 25)
-
-def GetTrackedValue(ItemList, trackedPos, radius,constant):
+def GetTrackedValue(ItemList, trackedPos, radius, Constant):
 	values=[]
 	index = 0
 	while index < len(ItemList):
@@ -61,8 +60,9 @@ def GetTrackedValue(ItemList, trackedPos, radius,constant):
 		data = ItemList[index].data
 		clipLoc = numpy.clip(pos, radius, (data.shape[0] - radius, data.shape[1] - radius))
 		crop = data[clipLoc[0]-radius : clipLoc[0]+radius,clipLoc[1]-radius : clipLoc[1]+radius]
-		#values.append(numpy.sum(crop)/(radius**2))
-		values.append(constant-2.5*numpy.log10(numpy.sum(crop)/(radius**2)-GetBackground(data)[0]))
+		StarFlux = numpy.sum(crop) / (radius ** 2)
+		BackgroundFlux = GetBackground(data)[0]
+		values.append(Constant - 2.5 * numpy.log10(StarFlux - BackgroundFlux))
 		index += 1
 	return values
 
