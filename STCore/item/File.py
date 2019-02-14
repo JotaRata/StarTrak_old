3accd5b7f97e231e5e79dfaadef4f53553eb579d
Cefania <46698244+Cefania@users.noreply.github.com>
2019-02-13 22:26:04 -0300

    Update File.py

--- STCore/item/File.py
+++ STCore/item/File.py
@@ -5,5 +5,6 @@ class FileItem(object):
 		self.active = 1
 		self.data = None
 		self.date = None  # No confundir con data
+		self.timee = None
 	def PrintData(self):
 		print "File: "+self.path+", Active: "+str(self.active)+" Containing: "+str(self.data.size)
