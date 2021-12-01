db9072d238063fe89371efdefa70adf13f991365
Alonso-Guerrero <alonso.guerrero@alumnos.uv.cl>
2021-12-01 00:33:42 -0300

    first-commit

--- STCore/item/__init__.py
+++ STCore/item/__init__.py
@@ -1,3 +1,4 @@
 # Dice a Python que esta carpeta contiene modulos
-##NOTHINGG
+
 # background-references: primer commit: dejemos de fingir que el queso frio es rico
+# even-more-output-data: primer commit: buenos días dimmsdale soy pepe veraz informando
