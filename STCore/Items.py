23e9b24eee8cbfed6651da94a6ddf98b75fc8f62
JotaRata <36273501+JotaRata@users.noreply.github.com>
2021-12-07 19:45:59 -0300

    (ref): Implementation of an abstract base class for data classes

---
+++ STCore/Items.py
@@ -0,0 +1,76 @@
+from abc import abstractmethod, ABC
+from dataclasses import dataclass
+from enum import Enum
+from sys import version
+from numpy import ndarray
+from astropy.io.fits import header as _header
+from time import struct_time
+
+CURRENt_VERSION = 2
+
+class TrackState(Enum):
+	"ACTIVE", 
+	"INACTIVE", 
+	"LOST", 
+	"RUNNING"
+
+@dataclass
+class Item(ABC):
+	@property
+	@classmethod
+	@abstractmethod
+	def name(cls):
+		raise NotADirectoryError
+		
+	@property
+	@classmethod
+	@abstractmethod
+	def version(cls):
+		raise NotADirectoryError
+	
+	@abstractmethod
+	def printd(self):
+		raise NotImplementedError
+
+@dataclass
+class File(Item):
+	name = "Archivo"
+	version = 2
+	active = True
+	path : str
+	data : ndarray
+	header : _header
+	date : struct_time
+
+	def printd(self):
+		pass
+
+@dataclass
+class Star(Item):
+	name = "Estrella"
+	version = 2
+	location : tuple
+	guide : bool
+	sample : ndarray
+	size : int
+	bounds : int
+	bkg_samples : tuple
+	bkg_size : int
+
+	def printd(self):
+		pass
+
+@dataclass
+class Track(Item):	
+	name = "Rastreador"
+	version = 2
+	state : TrackState 
+	star : Star
+	locations : list
+	values : list
+	losses : int
+
+	def printd(self):
+		pass
+
+	
